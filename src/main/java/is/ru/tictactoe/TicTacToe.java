2422762b0c39524099626672bc4a5868d2926974
Kristrun <kristrun12@ru.is>
2014-10-26 17:33:43 +0000

    fixing conflicts

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -2,13 +2,9 @@ package is.ru.tictactoe;
 
 public class TicTacToe
 {
-<<<<<<< HEAD
+
 	public static void main(String[] args) {
 		System.out.println("hello");
 	}
-=======
-	 public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
->>>>>>> 12573b1717ddd65b5f05c4016d2052e42a8a3e46
+
 }
